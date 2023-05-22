8dd7cee35603cb92b5de3c060d7b8010f27e4272
Pranshu321 <pranshujain0111@gmail.com>
2023-05-22 14:29:14 +0530

    New Starter Files and .env corrected

---
+++ app/layout.jsx
@@ -0,0 +1,32 @@
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/globals.css";
+
+
+export const metadata = {
+  title: "Promptcom",
+  description: "The community for prompts and ideas",
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
+};
+
+const layout = ({children}) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+}
+
+export default layout
---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+// import Feed from "@components/Feed";
+
+const Home = () => (
+  <section className="w-full flex-center flex-col">
+    <h1 className="head_text text-center">
+      Explore & Share
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center"> Stunning Prompts</span>
+    </h1>
+    <p className="desc text-center">
+      Promptcom is an open-source community for modern world to
+      discover, create and share creative prompts for modern AI tools.
+    </p>
+
+    {/* <Feed /> */}
+  </section>
+);
+
+export default Home;
