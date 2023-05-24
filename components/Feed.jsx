ee40b05db6a6c1ac622d35180d8394236ad3be94
Pranshu321 <pranshujain0111@gmail.com>
2023-05-24 14:53:41 +0530

    added the all profiles and copy func

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,29 +1,66 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PromptCard from "./PromptCard.jsx";
 
 const PromptCardList = ({ data, handleTagClick }) => {
-  return <div className="mt-16">
-    {data.map((prompt) => (
-      <PromptCard key={prompt.id} post={prompt} handleTagClick={handleTagClick} />
-    ))}
-  </div>;
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((prompt) => (
+        <PromptCard
+          key={prompt.id}
+          post={prompt}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
 };
 
 const Feed = () => {
-  const [searchValue, setSearchValue] = React.useState("");
-  const [prompts, setPrompts] = React.useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
 
-  function handleSearchChange(e) {}
+    const filterPrompts = (searchtext) => {
+      const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+      return allPosts.filter(
+        (item) =>
+          regex.test(item.creator.username) ||
+          regex.test(item.tag) ||
+          regex.test(item.prompt)
+      );
+    };
+
+    const handleSearchChange = (e) => {
+      clearTimeout(searchTimeout);
+      setSearchText(e.target.value);
+
+      // debounce method
+      setSearchTimeout(
+        setTimeout(() => {
+          const searchResult = filterPrompts(e.target.value);
+          setSearchedResults(searchResult);
+        }, 500)
+      );
+    };
+
+    const handleTagClick = (tagName) => {
+      setSearchText(tagName);
+
+      const searchResult = filterPrompts(tagName);
+      setSearchedResults(searchResult);
+    };
+
 
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await fetch("/api/prompt");
       const data = await res.json();
-      console.log(data[0].creator.image);
+      console.log(data);
 
-      setPrompts(data);
+      setAllPosts(data);
     };
 
     fetchPosts();
@@ -31,17 +68,17 @@ const Feed = () => {
 
   return (
     <section className="feed">
-      <form action="" className="relative w-full flex-center">
+      <div className="relative w-full">
         <input
           type="text"
           placeholder="Search tags or a username"
-          value={searchValue}
+          value={searchText}
           onChange={handleSearchChange}
           required
-          className="search_input peer text-[#2073E7] font-semibold"
+          className="search_input peer text-[#2073E7] font-bold"
         />
-      </form>
-      <PromptCardList data={prompts} handleTagClick={() => {}} />
+      </div>
+      <PromptCardList data={searchedResults} handleTagClick={handleTagClick} />
     </section>
   );
 };
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,35 +1,46 @@
 "use client";
 
-import React from 'react'
-import { useSession } from 'next-auth/react';
-import Image from 'next/image';
-import { usePathname , useRouter } from 'next/navigation';
+import React from "react";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = ({post , handleTagClick , handleEdit , handleDelete}) => {
-    const [copied , setCopied] = React.useState("");
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const [copied, setCopied] = React.useState("");
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
   const handleCopy = () => {
     setCopied(post.prompt);
     console.log(copied);
     navigator.clipboard.writeText(post.prompt);
     setTimeout(() => setCopied(false), 3000);
   };
+
+  const handleProfileClick = () => {
     console.log(post);
+
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+  // console.log(post);
   return (
-    <div className="relative block overflow-hidden rounded-lg border border-gray-100 p-4 sm:p-6 lg:p-8">
-      <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600" />
+    <div className="w-80 relative block overflow-hidden rounded-lg border border-gray-100 p-4 sm:p-6 lg:p-8">
+      <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 to-blue-600" />
       <div className="sm:flex sm:justify-between sm:gap-4">
         <div>
-          <h3
-            onClick={() => console.log(post.creator.image)}
-            className="text-lg font-bold text-gray-900 sm:text-xl"
-          >
+          <h3 className="text-lg font-bold text-gray-900 sm:text-xl">
             {post.creator.username}
           </h3>
           <p className="mt-1 text-xs font-medium text-gray-600">
             By {post.creator.email}
           </p>
         </div>
-        <div className="hidden sm:block sm:shrink-0">
+        <div
+          onClick={handleProfileClick}
+          className="hidden cursor-pointer sm:block sm:shrink-0"
+        >
           <Image
             src={post.creator.image}
             width={50}
@@ -40,7 +51,7 @@ const PromptCard = ({post , handleTagClick , handleEdit , handleDelete}) => {
         <div
           className="copy_btn"
           onClick={() => {
-            handleCopy
+            handleCopy;
           }}
         >
           <Image
@@ -60,17 +71,33 @@ const PromptCard = ({post , handleTagClick , handleEdit , handleDelete}) => {
         <p className="max-w-[40ch] text-sm text-gray-500">{post.prompt}</p>
       </div>
       <dl className="mt-6 flex gap-4 sm:gap-6">
-        <div className="flex flex-col-reverse">
-          <dt className="text-sm font-medium text-gray-600">Published</dt>
-          <dd className="text-xs text-gray-500">31st June, 2021</dd>
-        </div>
-        <div className="flex flex-col-reverse">
-          <dt className="text-sm font-medium text-gray-600">Reading time</dt>
-          <dd className="text-xs text-gray-500">3 minute</dd>
+        <div
+          onClick={() => handleTagClick && handleTagClick(post.tag)}
+          className="flex flex-col-reverse"
+        >
+          <dt className="text-sm font-medium text-gray-600">
+            Tags: {post.tag}
+          </dt>
         </div>
       </dl>
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 font-semibold flex-end gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient hover:underline cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient hover:underline cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default PromptCard
+export default PromptCard;
