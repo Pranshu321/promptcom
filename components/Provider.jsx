18abfc5bc95621eef81b4f44d72158e431e6d1a2
Pranshu321 <pranshujain0111@gmail.com>
2023-12-19 23:18:55 +0530

    Added the env and update the deploy

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,15 +1,10 @@
 "use client";
 
-import { SessionProvider } from 'next-auth/react'
-import React from 'react'
+import { SessionProvider } from "next-auth/react";
+import React from "react";
 
-const Provider = ({children , session}) => {
-  return (
-<SessionProvider session={session}>
-        {children}
-    </SessionProvider>
-    
-  )
-}
+const Provider = ({ children, session }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+};
 
-export default Provider
+export default Provider;
