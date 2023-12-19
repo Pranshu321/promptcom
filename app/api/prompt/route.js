0beff8abf285301ad7c5c80621df007ddbb9d3ed
Pranshu321 <pranshujain0111@gmail.com>
2023-12-19 23:51:34 +0530

    Added revalidate

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,9 +6,11 @@ export const GET = async (request) => {
     await connect();
 
     const prompts = await Prompt.find({}).populate("creator");
+    console.log("prompts", prompts);
 
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
+export const revalidate = 0;
